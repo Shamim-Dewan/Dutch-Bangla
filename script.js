d2e5f50686fddd23bbc395102c0f211a4c0067eb
Shamim-Dewan <570iamkhan@gmail.com>
2022-10-19 19:01:57 +0600

    js update

--- script.js
+++ script.js
@@ -12,56 +12,136 @@ function myFunction(){
 
 
 // own territory other outlets
+ //                               Cash In
 
 if(ownOrOther.value == "Own Territory Other Outlets" && trnx.value == "Cash In"){
   let fee = amount * 0.001;
-  charge.innerHTML = fee;
-  let  Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 200){
+    charge.value = 200;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 5){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
+//                                 Cash Out 
+
 
 if(ownOrOther.value == "Own Territory Other Outlets" && trnx.value == "Cash Out"){
   let fee = amount * 0.002;
-  charge.innerHTML = fee;
-  let Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 300){
+    charge.value = 300;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 5){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
+
+//                                 Fund Transfer
+
 if(ownOrOther.value == "Own Territory Other Outlets" && trnx.value == "Fund Transfer"){
   let fee = amount * 0.001;
-  charge.innerHTML = fee;
-  let Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 200){
+    charge.value = 200;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 5){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
 
 
 
 // other terrtory other outlets
 
+ //                               Cash In
+
 if(ownOrOther.value == "Other Territory Other Outlets" && trnx.value == "Cash In"){
   let fee = amount * 0.002;
-  charge.innerHTML = fee;
-  let  Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 400){
+    charge.value = 400;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 10){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
+
+
+//                                 Cash Out
 
 if(ownOrOther.value == "Other Territory Other Outlets" && trnx.value == "Cash Out"){
   let fee = amount * 0.004;
-  charge.innerHTML = fee;
-  let Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 500){
+    charge.value = 500;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 10){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
+
+//                                 Fund Transfer
+
 if(ownOrOther.value == "Other Territory Other Outlets" && trnx.value == "Fund Transfer"){
   let fee = amount * 0.002;
-  charge.innerHTML = fee;
-  let Total = fee + amount;
-  total.innerHTML = Total;
+  if(fee > 400){
+    charge.value = 400;
+    let  Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else if(fee < 10){
+    charge.value = 5;
+    let Total = +charge.value + amount;
+    total.value = Total;
+  }
+  else{
+    charge.value = fee;
+   let Total = +charge.value + amount;
+   total.value = Total;
+  }
 }
 
 }
 myFunction();
-
-
-
-
-
-
